1e8f217f7bb42c84d2d687e371d5e7819d3b1628
醉月含 <932897867@qq.com>
2018-09-03 18:11:34 +0800

    initial

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <a href="http://localhost:8080/#/list">前往List</a>
+    <router-link to="/list">to list</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+  template {
+    color: green;
+  }
+</style>
