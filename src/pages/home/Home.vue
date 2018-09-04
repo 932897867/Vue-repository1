458e7709c3bd55cea2d231658a89f0d9e917db0a
醉月含 <932897867@qq.com>
2018-09-04 16:41:44 +0800

    icons

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -5,17 +5,20 @@
     <span class="iconfont">&#xe650;</span>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeSwiper from './components/Swiper'
 import HomeHeader from './components/Header'
+import HomeIcons from './components/Icons'
 export default {
   name: 'Home',
   components: {
     HomeSwiper,
-    HomeHeader
+    HomeHeader,
+    HomeIcons
   }
 }
 </script>
